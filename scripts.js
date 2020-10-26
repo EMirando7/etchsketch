4886b4defc1cddf1cffc8cd94a5ed5fe6c5f8d36
Emiliano Miranda <emirando117@gmail.com>
2020-10-26 14:58:56 -0500

    remove condition of adding class last cell of row, remove some comments in styling

--- scripts.js
+++ scripts.js
@@ -1,17 +1,12 @@
 const container = document.querySelector('.container');
-for(let j = 0; j < 17; j++){  
+for(let j = 0; j < 16; j++){  
     for(let i = 1 ; i < 17; i ++){
     const subContainers = document.createElement('div');
     subContainers.innerHTML = `<div class="contentClass">${i}</div>`;
     subContainers.classList.add('subs');
     subContainers.classList.add('noWrap')
     container.appendChild(subContainers);
-        if(i === 1){
-            subContainers.classList.remove('noWrap');
-            subContainers.classList.add('wrap')
-        }
-        console.log(subContainers)
-    }
+    }    
 }    
 
 
