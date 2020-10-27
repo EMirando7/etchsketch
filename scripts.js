0834b064cf188ac24a2301149433bfcde2cd4368
Emiliano Miranda <emirando117@gmail.com>
2020-10-26 19:20:03 -0500

    add css, remove js add class

--- scripts.js
+++ scripts.js
@@ -2,9 +2,7 @@ const container = document.querySelector('.container');
 for(let j = 0; j < 16; j++){  
     for(let i = 1 ; i < 17; i ++){
     const subContainers = document.createElement('div');
-    subContainers.innerHTML = `<div class="contentClass">${i}</div>`;
     subContainers.classList.add('subs');
-    subContainers.classList.add('noWrap')
     container.appendChild(subContainers);
     }    
 }    
