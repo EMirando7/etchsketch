95e95dd93a62187badb4b4e54ff17d8d1ac2da28
Emiliano Miranda <emirando117@gmail.com>
2020-10-29 15:40:46 -0500

    fix function and css to add grid correctly

--- scripts.js
+++ scripts.js
@@ -1,12 +1,28 @@
 const container = document.querySelector('.container');
-for(let j = 0; j < 16; j++){  
-    for(let i = 1 ; i < 17; i ++){
-    const subContainers = document.createElement('div');
-    subContainers.classList.add('subs');
-    container.appendChild(subContainers);
+const btn = document.createElement('button');
+btn.addEventListener('click', function getNumber() {
+    let answer = prompt('Hey guy/gal, how many squares per side?');
+    runGame(answer);
+})
+document.body.insertBefore(btn, container);
+function runGame(foo) {
+    // debugger;
+    for(let j = 0; j < foo; j++){ 
+        for( let i = 0;  i < foo; i ++){
+            const subContainers = document.createElement('div');
+            subContainers.classList.add('subs');
+            container.appendChild(subContainers);
+            subContainers.addEventListener('mouseenter', () => {
+            subContainers.classList.add('cellColor')
+            })
+        }
+        let wrapCell = document.createElement('div');
+        wrapCell.classList.add('wrap');
+        container.appendChild(wrapCell)
+        container.lastChild.classList.add('wrap');
+        container.lastChild.classList.remove('subs')
+        console.log(wrapCell)
+    
     }    
-}    
-
-
-
-
+}
+runGame(2)
